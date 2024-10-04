c3e2321dace48e3f52dbcf366aa84b3c3528863e
Thiago <linktale2002@gmail.com>
2024-10-04 19:45:07 -0300

    Se añadieron las clases

---
+++ Escenarios del tp1/Biblioteca/src/Modelo/Biblioteca.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+/**
+ * Biblioteca
+ */
+public class Biblioteca {
+
+    public Biblioteca(){
+    }
+
+    public static void prestarLibro(libro: Libro, usuario: Usuario) {
+        
+    }
+}
