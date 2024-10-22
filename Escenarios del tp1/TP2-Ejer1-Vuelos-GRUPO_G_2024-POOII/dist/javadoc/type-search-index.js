0754550137de46c4e35e9b92695ecb15b3be55aa
krutkidaniel2020 <krutki.daniel2020@gmail.com>
2024-10-22 00:02:26 -0300

    Cambios de nombres

--- Escenarios del tp1/TP2-Ejer1-Vuelos-GRUPO_G_2024-POOII/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Clase","l":"Biblioteca"},{"p":"Clase","l":"Libro"},{"p":"Clase","l":"Prestamo"},{"p":"<Unnamed>","l":"Principal"},{"p":"Clase","l":"Usuario"}];updateSearchResults();
---
+++ Escenarios del tp1/TP2-Ejer2-Restaurante-GRUPO_G_2024-POOII/build/classes/Clases/Plato.rs
@@ -0,0 +1,1 @@
+Modelo.Plato
